703dc35228457bd7555bb8fd6fa3c5cd4f91b76f
Gustavo dos Santos Ribeiro <gustavo.ribeiro@presidencia.gov.br>
2024-08-28 16:40:35 -0300

    style: aplicado formatador ruff

--- academic/project/exemplo.py
+++ academic/project/exemplo.py
@@ -1,22 +1,28 @@
+"""Exemplo básico do Prof. Luan."""
+
 import tkinter as tk
-from tkinter import messagebox
 from pathlib import Path
+from tkinter import messagebox
 
 if Path('lista_de_tarefas.txt').is_file():
-    with open("lista_de_tarefas.txt", "w") as arquivo:
+    with Path('lista_de_tarefas.txt').open('w') as arquivo:
         arquivo.write('')
+
 
 # Função para adicionar uma nova tarefa à lista
 def adicionar_tarefa():
+    """Adicionar tarefa."""
     tarefa = entry_tarefa.get()
     if tarefa:
         lista_tarefas.insert(tk.END, tarefa)
         entry_tarefa.delete(0, tk.END)
     else:
-        messagebox.showwarning("Aviso", "Por favor, insira uma tarefa válida.")
+        messagebox.showwarning('Aviso', 'Por favor, insira uma tarefa válida.')
+
 
 # Função para editar a tarefa selecionada
 def editar_tarefa():
+    """Editar tarefa."""
     selecao = lista_tarefas.curselection()
     if selecao:
         indice = selecao[0]
@@ -26,42 +32,52 @@ def editar_tarefa():
             lista_tarefas.insert(indice, nova_tarefa)
             entry_tarefa.delete(0, tk.END)
         else:
-            messagebox.showwarning("Aviso", "Por favor, insira uma tarefa válida.")
+            messagebox.showwarning(
+                'Aviso',
+                'Por favor, insira uma tarefa válida.',
+            )
     else:
-        messagebox.showwarning("Aviso", "Selecione uma tarefa para editar.")
+        messagebox.showwarning('Aviso', 'Selecione uma tarefa para editar.')
+
 
 # Função para excluir a tarefa selecionada
 def excluir_tarefa():
+    """Excluir tarefa."""
     selecao = lista_tarefas.curselection()
     if selecao:
         indice = selecao[0]
         lista_tarefas.delete(indice)
         entry_tarefa.delete(0, tk.END)
     else:
-        messagebox.showwarning("Aviso", "Selecione uma tarefa para excluir.")
+        messagebox.showwarning('Aviso', 'Selecione uma tarefa para excluir.')
+
 
 # Função para salvar a lista de tarefas em um arquivo de texto
 def salvar_lista():
+    """Salvar lista."""
     tarefas = lista_tarefas.get(0, tk.END)
-    with open("lista_de_tarefas.txt", "w") as arquivo:
+    with Path('lista_de_tarefas.txt').open('w') as arquivo:
         for tarefa in tarefas:
-            arquivo.write(tarefa + "\n")
+            arquivo.write(tarefa + '\n')
+
 
 # Função para carregar a lista de tarefas de um arquivo de texto
 def carregar_lista():
+    """Carregar lista."""
     lista_tarefas.delete(0, tk.END)
     try:
-        with open("lista_de_tarefas.txt", "r") as arquivo:
+        with Path('lista_de_tarefas.txt').open() as arquivo:
             for linha in arquivo:
                 lista_tarefas.insert(tk.END, linha.strip())
     except FileNotFoundError:
         pass
 
+
 # Configuração da janela principal
 root = tk.Tk()
-root.title("Lista de Tarefas")
+root.title('Lista de Tarefas')
 # dimensões da janela
-root.geometry("300x200+100+100")
+root.geometry('300x200+100+100')
 
 # Criando e configurando a lista de tarefas
 lista_tarefas = tk.Listbox(root, selectmode=tk.SINGLE)
@@ -72,11 +88,15 @@ entry_tarefa = tk.Entry(root)
 entry_tarefa.pack(pady=5)
 
 # Botões
-btn_adicionar = tk.Button(root, text="Adicionar Tarefa", command=adicionar_tarefa)
-btn_editar = tk.Button(root, text="Editar Tarefa", command=editar_tarefa)
-btn_excluir = tk.Button(root, text="Excluir Tarefa", command=excluir_tarefa)
-btn_salvar = tk.Button(root, text="Salvar Lista", command=salvar_lista)
-btn_carregar = tk.Button(root, text="Carregar Lista", command=carregar_lista)
+btn_adicionar = tk.Button(
+    root,
+    text='Adicionar Tarefa',
+    command=adicionar_tarefa,
+)
+btn_editar = tk.Button(root, text='Editar Tarefa', command=editar_tarefa)
+btn_excluir = tk.Button(root, text='Excluir Tarefa', command=excluir_tarefa)
+btn_salvar = tk.Button(root, text='Salvar Lista', command=salvar_lista)
+btn_carregar = tk.Button(root, text='Carregar Lista', command=carregar_lista)
 
 btn_adicionar.pack()
 btn_editar.pack()
