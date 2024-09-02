2cddb21bff509986bd712281d4e3444c0a753391
Gustavo dos Santos Ribeiro <gustavo.ribeiro@presidencia.gov.br>
2024-09-02 18:02:32 -0300

    feat: adicionado estrutura base do formulário para alunos

--- academic/project/views/pages.py
+++ academic/project/views/pages.py
@@ -8,9 +8,40 @@ def main_view(e: ft.ControlEvent) -> ft.Control:
     logging.debug(e)
     return ft.View(
         route='/',
+        horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+        vertical_alignment = ft.MainAxisAlignment.CENTER,
+        padding=0,
         controls=[
-            ft.Text('Página principal'),
+            ft.Container(
+                content=ft.Column(
+                    controls=[
+                        ft.TextField(
+                            label='Nome do aluno',
+                        ),
+                        ft.TextField(
+                            label='Turma',
+                        ),
+                        ft.TextField(
+                            label='Nota',
+                        ),
+                        ft.Row(
+                            controls=[
+                                ft.TextButton('Adicionar nota'),
+                                ft.TextButton('Novo aluno'),
+                            ],
+                        ),
+                    ],
+                ),
+            ),
         ],
+    )
+
+
+def visualizar_view(e: ft.ControlEvent) -> ft.ControlEvent:
+    """Visualizar view."""
+    logging.debug(e)
+    return ft.View(
+        route='/visualizar',
     )
 
 
